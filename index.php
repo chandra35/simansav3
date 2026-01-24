6f5d2f7b9d03389b3559486893f430b5fe7ae980
chandra35 <chandra35ok@gmail.com>
2026-01-24 22:18:48 +0700

    Add root index.php and .htaccess for non-public webroot setup

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ * 
+ * This file redirects all requests to the public folder.
+ * Used when webroot cannot be changed to /public
+ */
+
+// Redirect to public folder
+require __DIR__.'/public/index.php';
