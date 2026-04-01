5dc388d5cdc09ab5e27742bb81146ee440f6c788
chandra35 <chandra35ok@gmail.com>
2026-04-01 13:30:42 +0700

    Add SIMANSA global visual theme foundation

--- resources/views/vendor/adminlte/partials/cwrapper/cwrapper-default.blade.php
+++ resources/views/vendor/adminlte/partials/cwrapper/cwrapper-default.blade.php
@@ -19,7 +19,11 @@
     @hasSection('content_header')
         <div class="content-header">
             <div class="{{ config('adminlte.classes_content_header') ?: $def_container_class }}">
-                @yield('content_header')
+                <div class="simansa-page-hero">
+                    <div class="simansa-page-hero__body">
+                        @yield('content_header')
+                    </div>
+                </div>
             </div>
         </div>
     @endif
