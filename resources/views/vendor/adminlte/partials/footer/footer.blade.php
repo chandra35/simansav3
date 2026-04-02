500685aac92056eb0ff6b0e155a4e506d3a7b6dc
chandra35 <chandra35ok@gmail.com>
2026-04-02 08:06:37 +0700

    Add SIMANSA global footer

--- resources/views/vendor/adminlte/partials/footer/footer.blade.php
+++ resources/views/vendor/adminlte/partials/footer/footer.blade.php
@@ -1,3 +1,17 @@
-<footer class="main-footer">
-    @yield('footer')
+<footer class="main-footer simansa-footer">
+    <div class="simansa-footer__inner">
+        <div class="simansa-footer__brand">
+            <strong>SIMANSA</strong>
+            <span>Sistem Informasi MAN 1 Metro</span>
+        </div>
+        <div class="simansa-footer__meta">
+            <span>&copy; 2026 TIM IT MAN 1 Metro</span>
+        </div>
+    </div>
+
+    @hasSection('footer')
+        <div class="simansa-footer__extra">
+            @yield('footer')
+        </div>
+    @endif
 </footer>
