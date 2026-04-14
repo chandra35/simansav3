131c3ffe2be907e50dc6976a5378316a4f44e66f
chandra35 <chandra35ok@gmail.com>
2026-04-14 10:57:55 +0700

    fix(menu): hide admin smartq menu from siswa to remove duplicate entry

--- config/adminlte.php
+++ config/adminlte.php
@@ -477,6 +477,7 @@ return [
             'text' => 'SMART-Q Unggulan',
             'icon' => 'fas fa-fw fa-star text-warning',
             'route' => 'admin.smartq.index',
+            'can' => 'admin-access',
             'active' => ['admin/smartq*'],
         ],
         
